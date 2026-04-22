3fab418e1ef027209bddb88fdb234da1150ed3f9
ultxvampi <pilycerdas21@gmail.com>
2026-04-22 14:23:34 -0600

    Quiz 3 D:

--- quizz/quizz02/quiz02_MPilarCerdas.cpp
+++ quizz/quizz02/quiz02_MPilarCerdas.cpp
@@ -1,44 +1,49 @@
 #include <iostream>
 #include <string>
 using namespace std;
-/// Vagones del tren
+/// Vagones del tren:b
 
-void Vagones(int vagones[], int numVagones){
+void Vagon(int vagones[], int numVagones){
   for (int i = 0; i <= numVagones; i++){
     vagones[i] = 0; // los vagones inician en 0
   }
 }
-void carga(int vagones[, int r, int v]){
-  for(int i=1; 1 <= r, i++){
-    vagones[x] += v; // agregarle la carga al vagón
+void carga(int vagones[], int r, int v){
+  for(int i=1; 1 <= r; i++){
+    vagones[i] += v; // agregarle la carga al vagón
   }
 }
 void operaciones(int vagones[], int numVagones){
   int numOperaciones;
   cin >> numOperaciones;
-  for ( i = 0; i < numOperaciones; i++){
-    strig operacion;
-    cout << "C o A, solo mayusculas"
+    string operacion;
+    cout << "C o A, solo mayusculas" << endl;
     cin >> operacion;
-    if (operacion == "A"){
+    if (operacion == "A"){ //Si la operacion es A da la carga de los vagones
       int v,r;
-      cout << "Ingrese los valores " << 
+      cout << "Ingrese los valores " << endl;
       cin >> v >> r;
       carga(vagones,v,r);  
      }
-     if(operacion == "C"){
+     if(operacion == "C"){ //Si la operacion es C
       int r;
       cin >> r;
       cout << vagones[r] << endl;
      }
-  }
-}
 
-int main{
+}
+void consultar(int vagones[], int r){
+    cout << vagones[r] << endl; // deberia de decir los vagones con su carga
+}
+int main(){
   int numVagones;
   cin >> numVagones;
   int vagones[numVagones + 1];
-  Vagones(vagones,numVagones);
+  Vagon(vagones, numVagones);
   operaciones(vagones, numVagones);
+  consultar(vagones, numVagones);
   return 0;
 }
+//sospecho q no lo entendí bien y no da lo que debería pero
+//mil disculpassssssssssssssssss :"((, porq lo meto en la terminal y me deja ponervalores
+// pero nada q ver con lo q debería de ser (creop)
