61e1c5965380bb92797bfef38670384f79a0cf4d
ultxvampi <pilycerdas21@gmail.com>
2026-09-08 09:11:29 -0600

    Primer parcial 2

--- 2Semestre/Exame/1Parcial/parte1_PilarCerdas.cpp
+++ 2Semestre/Exame/1Parcial/parte1_PilarCerdas.cpp
@@ -15,23 +15,18 @@ class Medicamento{
     int stock;
 
  public:
-  Medicamento(string nombre_med){
+  Medicamento(){
     this-> nombre_med = "Sin nombre";
     this-> precioUnitario = 0;
     this-> stock = 0;
   }
 
-  Medicamento(string nombre_med){
-    this->nombre_med = nombre;
-  }
-
-    Medicamento(int precioUnitario){
+  Medicamento(string nombre_med, int precioUnitario, int stock  ){
+    this->nombre_med = nombre_med;
+    this->precioUnitario = precioUnitario;
     this->stock = stock;
-  }
-
-    Medicamento(int stock){
-    this->stock = stock;
-  }
+    }
+    
 
   string getNombre(){
     return nombre_med;
@@ -41,12 +36,13 @@ class Medicamento{
     if(cantidad <= stock){
         stock -= cantidad;
         return true;
-    } else return false;
+    } 
+    return false;
 
  }
 
  int valorInventario(){
-    return precioUnitacio * stock;
+    return precioUnitario * stock;
  }
 
 };
@@ -57,53 +53,68 @@ int main(){
     cin >> M;
     cin.ignore();
 
- vector<Medicamento>;
+ vector<Medicamento> inventario;
+ queue<pair<string,int>> pedidos;
 
- queue<pair<string,in>>;
-
- for (int i = 0; i< M ; i++ ){
+ for (int i = 0; i < M ; i++ ){
     string comando;
     cin >> comando;
-
- }
 
  if (comando == "agregar"){
     string nombre;
     int precio, stock;
-
     cin >> nombre >> precio >> stock;
-
     inventario.push_back(Medicamento(nombre, precio, stock));
- } 
+    cout <<"Listo."<< endl; //esto es para ver yo si sirve en la terminal:c
+    }  
+
  else if (comando == "pedido"){
     string nombre;
     int cantidad;
     cin >> nombre >> cantidad;
     pedidos.push({nombre, cantidad});
-
  }
 
  else if (comando == "procesar"){
     if(pedidos.empty()){
         cout << "No hay pedidos pendientes." << endl;
+    } else {
+        string nombre = pedidos.front().first;
+        int cantidad =  pedidos.front().second;
+        pedidos.pop();
+
+        // buscar inventario
+
+        bool encontrado = false;
+        for(auto& medicina : inventario){
+            if(medicina.getNombre()== nombre){
+                encontrado = true;
+            }
+            if(medicina.despachar(cantidad)){
+                cout << "Despachado: " << nombre << " x " << cantidad << endl;
+            } else{
+                cout << "Stock insuficiente." << endl;
+            }
+            break;
+        }   if(!encontrado){
+             cout << "No hay." << endl;
+        }
     }
  }
-
-
- ///COMANDOS
-
- //Agregar nombre precio stock, crea Medicamento con esos datos
-
- // Pedido nombre cantidad, registra pedido de cantidad unidades del medicamento nombre
-
- //Procesar atiende el pedido más antiguo en la fila, lo retira, lo busca y lo despacha
- //Si el despacho fue exitoso imprime Despachado: NOMBRE X CANTIDAD
-
- //Si no habia tanto stock imprime Stock insuficiente: NOMBRE
-
- //Si no hay pedidps pendientes: No hay pedidos pendientes
-
- //Inventario recorre el inventario, suma el valor Inventario() de cada med e imprime
- //Valor del inventario: x
+ else if(comando == "inventario") {
+    int total = 0;
+    for(auto& medicina : inventario){
+        total += medicina.valorInventario();
+    }
+    cout << "Valor del inventario: " << total << endl;
+ }
+ return 0;
+}
 }
 
+// CREO QUE NO SIRVEEEEEEEEEEEEEE, bueno al menos a mí no me deja tirar más (M)
+// cuando hago el ejemplo de entrada y eso, pero si como q imprime lo de agregar, al menos
+// no me da errores en todo :,,,,c
+// igual en la parte del int tengo una confusioooon con los nombres, porq di si uno hace
+// string nombre; debería de estar en ese comando(¿) pero ns si podia poner eso o 
+// obligatoriamente nombre_med yyyyyy pues intente cambiarlo pero m mando como mil erroresssss
